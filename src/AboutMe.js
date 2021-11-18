eaef13f1c45e4fbfd5abb14f954683b5012d9e68
Taniya Sterling <sterlingtaniya@gmail.com>
2021-11-18 17:30:59 -0500

    updating

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,24 +4,30 @@ import './AboutMe.scss'
 
 const MyComponent = () => {
     return(
-       <div>
-        <div class="columns">
-            <div class="column">
-                First column
-            </div>
-            <div class="column">
-                Second column
-            </div>
-            <div class="column">
-                Third column
-            </div>
-            <div class="column">
-                Fourth column
-            </div>
-        </div>
-           </div> 
+        <div className='container columns is-multiline'>
+        <div class="card column is-half is-offset-one-quarter" id ='about-me-card'>
+        <div class="card-content">
+    <p class="title">About Me</p>
+    <ul class = "about-me-list">          
+            <li class = "boxy-list" id = "boxy-list">I am a South Florida native studying software engineering at OU</li>
+            <li class = "boxy-list">I grew up watching my mom build desktops from the ground up and playing video games with my dad, which are big reasons for my interest in programming</li>
+            <li class = "boxy-list">my personality type is INFP</li>
+            <li class = "boxy-list">I am a certified phlebotomist</li>
+        </ul>
+    <p class="subtitle">
+     Taniya Sterling
+    </p>
+  </div>
+</div>
+</div>
     )
-
 }
 
 export default MyComponent;
+
+{/* <div class = 'container'>
+        <div  id= "about-me-boxy"> 
+       <div className='title'>Taniya Sterling</div>
+     
+   </div>
+           </div>  */}
--- src/Main.js
+++ src/Main.js
@@ -5,8 +5,10 @@ import './main.scss'
 const MainComponent = () => {
     return(
         <div class="container">
-  <h1 class="title section  has-text-centered" id ='name'>Taniya Sterling</h1>
-  <div class="subtitle section has-text-centered">
+  <div>
+    <h1 class="title section  has-text-centered" id ='name'>Taniya Sterling</h1>
+  </div>  
+  <div class="subtitle section has-text-centered">    
     <h2 >
   "You are the light of the world. A city cannot be hid when located on a mountain." - Matthew 5:14
   </h2>
