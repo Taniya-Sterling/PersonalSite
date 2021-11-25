ab6d3680be7dae539f9a77279eab672d6f226f8d
Taniya Sterling <sterlingtaniya@gmail.com>
2021-11-24 20:52:36 -0500

    my about me update, responsive

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,12 +4,14 @@ import './AboutMe.scss'
 
 const MyComponent = () => {
     return(
-        <div className='container columns is-multiline'>
-        <div class="card column is-half is-offset-one-quarter" id ='about-me-card'>
+        <div className='container  '>
+        <div class="card column is-full is-size-6-mobile" id ='about-me-card'>
         <div class="card-content">
-    <p class="title">About Me</p>
+    <p class="title is-size-5-mobile">About Me</p>
+    <i class="material-icons">code</i>
+
     <ul class = "about-me-list">          
-            <li class = "boxy-list">I am a South Florida native studying software engineering at OU</li>
+            <li class = "boxy-list">I am a South Florida native who studied software development at Fullstack Academy</li><br/>
             <li class = "boxy-list">I grew up watching my mom build desktops from the ground up and playing video games with my dad, which are big reasons for my interest in programming</li>
             <li class = "boxy-list">my personality type is INFP</li>
             <li class = "boxy-list">I am a certified phlebotomist</li>
